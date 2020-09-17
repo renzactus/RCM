b3b6aeedb5d7a90fc765f25d6a3e2eb7b689b36e
renzactus <renzactus@gmail.com>
2020-09-17 17:09:04 -0300

    16--9
    actualizado en clase de programacion

--- Atila/Atila/MySQL.vb
+++ Atila/Atila/MySQL.vb
@@ -1,8 +1,8 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class MySQL
     Public Consulta As String
-    Private ComandoSql As New MySqlCommand
-    Dim MysqlConnString As String = "server=localhost;port=3307;database=Atila;user=root;password=root;"
+    Public ComandoSql As New MySqlCommand
+    Dim MysqlConnString As String = "server=localhost;database=Atila1;user=root;password=root;" 'port=3307;
     Public Conexion As MySqlConnection = New MySqlConnection(MysqlConnString)
     Public Sub Probarconexion() 'PROBAR CONEXION DE UNA TABLA
         Try
@@ -16,7 +16,7 @@ Public Class MySQL
     Public Sub InsertarDatos() 'INSERTAR DATOS EN UNA TABLA DE LA BASE DE DATOS
         ComandoSql = New MySqlCommand(Consulta)
         Conexion.Open()
-        ComandoSql()
+        ComandoSql.Connection = Conexion
         ComandoSql.ExecuteNonQuery()
         Conexion.Close()
 
