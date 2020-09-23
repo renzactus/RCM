89f3693d006ff62e085986f122dc006ecf545d93
renzactus <renzactus@gmail.com>
2020-09-23 02:15:35 -0300

    Revert "17-9"
    This reverts commit 4cd6c65ce71ed9168b96af9dc1496b7f0b33ab3f.

--- Atila/Atila/Reservar.vb
+++ Atila/Atila/Reservar.vb
@@ -1,40 +1,45 @@
 ﻿Public Class Reservar
-    Dim booleanTelefonos As Boolean
+    Dim contadorTelefonos As Integer
     Dim mysql As New MySQL
-    Dim telefonos As String
+    Private Sub cuadroNombre_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNombre.TextChanged
+
+    End Sub
+
     Private Sub Reservar_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         mysql.Probarconexion()
+        contadorTelefonos = 0
     End Sub
 
     Private Sub botonAgregarDatos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgregarDatos.Click
-        If txtTelefono2.Text <> "" Then
-            telefonos = txtTelefono1.Text & "|" & txtTelefono2.Text
-        Else
-            telefonos = txtTelefono1.Text
-        End If
-        mysql.Consulta = "insert into clientes (cedula,nombre,telefonos,direccion) values('" & txtCedula.Text & "','" & txtNombre.Text & "','" & telefonos & "','" & txtDireccion.Text & "')"
+        mysql.Consulta = "insert into clientes (nombre,telefonos,direccion) values('" & txtNombre.Text & "',200,2)"
         mysql.Probarconexion()
         mysql.InsertarDatos()
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgregarTelefonos.Click
-        'PARA PONER VISIBLE EL txtTelefonos2 Y MOVER DE LUGAR txtDireccion y lblDireccion
-        If booleanTelefonos = False Then
+        '
+        If contadorTelefonos = 0 Then
             txtTelefono2.Visible = True
-            btnAgregarTelefonos.Text = "-"
-            booleanTelefonos = True
-            txtDireccion.Location = New Point(txtDireccion.Location.X, txtDireccion.Location.Y + 27)
-            lblDireccion.Location = New Point(lblDireccion.Location.X, lblDireccion.Location.Y + 27)
-        ElseIf booleanTelefonos = True Then
-            txtTelefono2.Visible = False
-            btnAgregarTelefonos.Text = "+"
-            txtTelefono2.Text = ""
-            booleanTelefonos = False
-            txtDireccion.Location = New Point(txtDireccion.Location.X, txtDireccion.Location.Y - 27)
-            lblDireccion.Location = New Point(lblDireccion.Location.X, lblDireccion.Location.Y - 27)
+            btnRestarTelefonos.Visible = True
+            contadorTelefonos = contadorTelefonos + 1
+        ElseIf contadorTelefonos = 1 Then
+            txtTelefono3.Visible = True
+            btnAgregarTelefonos.Enabled = False
+            contadorTelefonos = contadorTelefonos + 1
         End If
 
+    End Sub
 
-
+    Private Sub botonRestarTelefonos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRestarTelefonos.Click
+        If contadorTelefonos = 1 Then
+            txtTelefono2.Visible = False
+            btnAgregarTelefonos.Enabled = True
+            contadorTelefonos = contadorTelefonos - 1
+        ElseIf contadorTelefonos = 2 Then
+            txtTelefono3.Visible = False
+            btnAgregarTelefonos.Enabled = True
+            contadorTelefonos = 0
+            contadorTelefonos = contadorTelefonos - 1
+        End If
     End Sub
 End Class
