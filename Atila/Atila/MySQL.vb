29671fb9e948b2cbdb9d829b7596c1d00840de62
renzactus <renzactus@gmail.com>
2020-09-23 02:15:58 -0300

    Revert "16--9"
    This reverts commit b3b6aeedb5d7a90fc765f25d6a3e2eb7b689b36e.

--- Atila/Atila/MySQL.vb
+++ Atila/Atila/MySQL.vb
@@ -1,8 +1,8 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class MySQL
     Public Consulta As String
-    Public ComandoSql As New MySqlCommand
-    Dim MysqlConnString As String = "server=localhost;database=Atila1;user=root;password=root;" 'port=3307;
+    Private ComandoSql As New MySqlCommand
+    Dim MysqlConnString As String = "server=localhost;port=3307;database=Atila;user=root;password=root;"
     Public Conexion As MySqlConnection = New MySqlConnection(MysqlConnString)
     Public Sub Probarconexion() 'PROBAR CONEXION DE UNA TABLA
         Try
@@ -16,7 +16,7 @@ Public Class MySQL
     Public Sub InsertarDatos() 'INSERTAR DATOS EN UNA TABLA DE LA BASE DE DATOS
         ComandoSql = New MySqlCommand(Consulta)
         Conexion.Open()
-        ComandoSql.Connection = Conexion
+        ComandoSql()
         ComandoSql.ExecuteNonQuery()
         Conexion.Close()
 
--- Atila/Atila/Reservar.vb
+++ Atila/Atila/Reservar.vb
@@ -1,45 +1,16 @@
 ﻿Public Class Reservar
-    Dim contadorTelefonos As Integer
     Dim mysql As New MySQL
-    Private Sub cuadroNombre_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNombre.TextChanged
+    Private Sub cuadroNombre_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cuadroNombre.TextChanged
 
     End Sub
 
     Private Sub Reservar_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         mysql.Probarconexion()
-        contadorTelefonos = 0
     End Sub
 
-    Private Sub botonAgregarDatos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgregarDatos.Click
-        mysql.Consulta = "insert into clientes (nombre,telefonos,direccion) values('" & txtNombre.Text & "',200,2)"
+    Private Sub botonAgregarDatos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles botonAgregarDatos.Click
+        mysql.Consulta = "insert into inventario (descripcion,precio,cantidad) values('item1',200,2)"
         mysql.Probarconexion()
         mysql.InsertarDatos()
-    End Sub
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgregarTelefonos.Click
-        '
-        If contadorTelefonos = 0 Then
-            txtTelefono2.Visible = True
-            btnRestarTelefonos.Visible = True
-            contadorTelefonos = contadorTelefonos + 1
-        ElseIf contadorTelefonos = 1 Then
-            txtTelefono3.Visible = True
-            btnAgregarTelefonos.Enabled = False
-            contadorTelefonos = contadorTelefonos + 1
-        End If
-
-    End Sub
-
-    Private Sub botonRestarTelefonos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRestarTelefonos.Click
-        If contadorTelefonos = 1 Then
-            txtTelefono2.Visible = False
-            btnAgregarTelefonos.Enabled = True
-            contadorTelefonos = contadorTelefonos - 1
-        ElseIf contadorTelefonos = 2 Then
-            txtTelefono3.Visible = False
-            btnAgregarTelefonos.Enabled = True
-            contadorTelefonos = 0
-            contadorTelefonos = contadorTelefonos - 1
-        End If
     End Sub
 End Class
