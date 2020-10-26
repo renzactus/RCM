80cfd6996a6a0d899224452ffa1da03bde774020
renzactus <renzactus@gmail.com>
2020-10-26 16:19:16 -0300

    ...
    Avances en inventario

--- Atila/Atila/Inventario.Designer.vb
+++ Atila/Atila/Inventario.Designer.vb
@@ -26,9 +26,17 @@ Partial Class Inventario
         Me.dgvInventario = New System.Windows.Forms.DataGridView()
         Me.Descripcion = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Cantidad = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.conf = New System.Windows.Forms.DataGridViewButtonColumn()
-        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.btnEditarCantidad = New System.Windows.Forms.Button()
+        Me.btnAgregarDatos = New System.Windows.Forms.Button()
+        Me.txtDescripcion = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.nudCantidad = New System.Windows.Forms.NumericUpDown()
+        Me.epError = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.btnBorrar = New System.Windows.Forms.Button()
         CType(Me.dgvInventario, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nudCantidad, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.epError, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'dgvInventario
@@ -38,14 +46,13 @@ Partial Class Inventario
         Me.dgvInventario.AllowUserToResizeColumns = False
         Me.dgvInventario.AllowUserToResizeRows = False
         Me.dgvInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
-        Me.dgvInventario.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Descripcion, Me.Cantidad, Me.conf})
-        Me.dgvInventario.Location = New System.Drawing.Point(63, 48)
+        Me.dgvInventario.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Descripcion, Me.Cantidad})
+        Me.dgvInventario.Location = New System.Drawing.Point(23, 47)
         Me.dgvInventario.MultiSelect = False
         Me.dgvInventario.Name = "dgvInventario"
         Me.dgvInventario.ReadOnly = True
-        Me.dgvInventario.RowHeadersVisible = False
         Me.dgvInventario.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.dgvInventario.Size = New System.Drawing.Size(327, 114)
+        Me.dgvInventario.Size = New System.Drawing.Size(230, 114)
         Me.dgvInventario.TabIndex = 23
         '
         'Descripcion
@@ -63,36 +70,103 @@ Partial Class Inventario
         Me.Cantidad.ReadOnly = True
         Me.Cantidad.Width = 60
         '
-        'conf
+        'btnEditarCantidad
         '
-        Me.conf.Frozen = True
-        Me.conf.HeaderText = ""
-        Me.conf.Name = "conf"
-        Me.conf.ReadOnly = True
-        Me.conf.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
-        Me.conf.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
-        Me.conf.Width = 40
+        Me.btnEditarCantidad.Enabled = False
+        Me.btnEditarCantidad.Location = New System.Drawing.Point(269, 47)
+        Me.btnEditarCantidad.Name = "btnEditarCantidad"
+        Me.btnEditarCantidad.Size = New System.Drawing.Size(85, 43)
+        Me.btnEditarCantidad.TabIndex = 24
+        Me.btnEditarCantidad.Text = "Editar Seleccionado"
+        Me.btnEditarCantidad.UseVisualStyleBackColor = True
         '
-        'ContextMenuStrip1
+        'btnAgregarDatos
         '
-        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
-        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        Me.btnAgregarDatos.Location = New System.Drawing.Point(450, 134)
+        Me.btnAgregarDatos.Name = "btnAgregarDatos"
+        Me.btnAgregarDatos.Size = New System.Drawing.Size(77, 43)
+        Me.btnAgregarDatos.TabIndex = 25
+        Me.btnAgregarDatos.Text = "Agregar Datos"
+        Me.btnAgregarDatos.UseVisualStyleBackColor = True
+        '
+        'txtDescripcion
+        '
+        Me.txtDescripcion.Location = New System.Drawing.Point(444, 47)
+        Me.txtDescripcion.Name = "txtDescripcion"
+        Me.txtDescripcion.Size = New System.Drawing.Size(100, 20)
+        Me.txtDescripcion.TabIndex = 26
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(464, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 27
+        Me.Label1.Text = "Descripcion"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(464, 77)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 31
+        Me.Label3.Text = "Cantidad"
+        '
+        'nudCantidad
+        '
+        Me.nudCantidad.Location = New System.Drawing.Point(467, 93)
+        Me.nudCantidad.Maximum = New Decimal(New Integer() {300, 0, 0, 0})
+        Me.nudCantidad.Name = "nudCantidad"
+        Me.nudCantidad.Size = New System.Drawing.Size(53, 20)
+        Me.nudCantidad.TabIndex = 32
+        '
+        'epError
+        '
+        Me.epError.ContainerControl = Me
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Enabled = False
+        Me.btnBorrar.Location = New System.Drawing.Point(269, 118)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(85, 43)
+        Me.btnBorrar.TabIndex = 33
+        Me.btnBorrar.Text = "Borrar Seleccionado"
+        Me.btnBorrar.UseVisualStyleBackColor = True
         '
         'Inventario
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(674, 353)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Controls.Add(Me.nudCantidad)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDescripcion)
+        Me.Controls.Add(Me.btnAgregarDatos)
+        Me.Controls.Add(Me.btnEditarCantidad)
         Me.Controls.Add(Me.dgvInventario)
         Me.Name = "Inventario"
         Me.Text = "Inventario"
         CType(Me.dgvInventario, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nudCantidad, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.epError, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents dgvInventario As System.Windows.Forms.DataGridView
+    Friend WithEvents btnEditarCantidad As System.Windows.Forms.Button
     Friend WithEvents Descripcion As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Cantidad As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents conf As System.Windows.Forms.DataGridViewButtonColumn
-    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents btnAgregarDatos As System.Windows.Forms.Button
+    Friend WithEvents txtDescripcion As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents nudCantidad As System.Windows.Forms.NumericUpDown
+    Friend WithEvents epError As System.Windows.Forms.ErrorProvider
+    Friend WithEvents btnBorrar As System.Windows.Forms.Button
 End Class
--- Atila/Atila/Inventario.vb
+++ Atila/Atila/Inventario.vb
@@ -1,16 +1,61 @@
 ﻿Public Class Inventario
     Dim mysql As New MySQL
+    Dim ibImprevisto As String
+    Dim booleanSeleccionada As Boolean
+    Dim FilaSeleccionada As Integer
+    Dim datosInventario As DataTable
     'Constructor
     Private Sub Inventario_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         MostrarInventarioEndgv()
+        dgvInventario.Rows(0).Selected = False
+        btnEditarCantidad.Enabled = False
+        btnBorrar.Enabled = False
     End Sub
     'Metodos Utilizados
     Private Sub MostrarInventarioEndgv()
+        dgvInventario.Rows.Clear()
         mysql.Consultar("select * from inventario")
-        For i = 0 To mysql.Resultado.Rows.Count - 1
-            dgvInventario.Rows.Add(mysql.Resultado.Rows(i).Item("descripcion"), mysql.Resultado.Rows(i).Item("cantidad"))
+        datosInventario = mysql.Resultado
+        For i = 0 To datosInventario.Rows.Count - 1
+            dgvInventario.Rows.Add(datosInventario.Rows(i).Item("descripcion"), datosInventario.Rows(i).Item("cantidad"))
         Next
     End Sub
+    Private Sub SaberFilaSeleccionada()
+
+        If dgvInventario.SelectedRows.Count > 0 Then
+            FilaSeleccionada = dgvInventario.CurrentRow.Index
+        End If
+    End Sub
+    'Agregar Inventario
+    Private Sub ChequearYAgregarInventario()
+        If txtDescripcion.Text = "" Then
+            avisarSiEstaVacio(txtDescripcion)
+            avisarSiEstaVacio(nudCantidad)
+        Else
+            mysql.InsertarDatos("insert into inventario (descripcion,cantidad) values ('" & txtDescripcion.Text & "'," & nudCantidad.Value & ")")
+            MostrarInventarioEndgv()
+        End If
+    End Sub
+    Private Sub avisarSiEstaVacio(ByVal componente As Object)
+        If componente.Text = "" Then
+            epError.SetError(componente, "Porfavor, Complete los datos de " & componente.Name.Substring(3))
+        Else
+            epError.SetError(componente, "")
+        End If
+    End Sub
+    'Borrar inventarios
+    Private Sub borrarInventario()
+        SaberFilaSeleccionada()
+        mysql.Consultar("select ID_INVENTARIO from utiliza where ID_INVENTARIO=" & datosInventario.Rows(FilaSeleccionada).Item("ID_INVENTARIO"))
+        If mysql.Resultado.Rows.Count > 0 Then
+            epError.SetError(dgvInventario, "Este Inventario no se puede eliminar porque se ha utilizado")
+        Else
+            mysql.InsertarDatos("delete from inventario where ID_INVENTARIO=" & datosInventario.Rows(FilaSeleccionada).Item("ID_INVENTARIO"))
+            MostrarInventarioEndgv()
+        End If
+    End Sub
+
+    'Enventos
 
     Private Sub dgvInventario_CellMouseEnter(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgvInventario.CellMouseEnter
         If e.ColumnIndex = 2 And e.RowIndex <> -1 Then
@@ -26,7 +71,51 @@
 
     Private Sub dgvInventario_CellMouseClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellMouseEventArgs) Handles dgvInventario.CellMouseClick
         If e.ColumnIndex = 2 And e.RowIndex <> -1 Then
-            ContextMenuStrip.Show()
         End If
+    End Sub
+
+    Private Sub btnEditarCantidad_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEditarCantidad.Click
+        SaberFilaSeleccionada()
+        ibImprevisto = InputBox("Editar Cantidad de " & datosInventario.Rows(FilaSeleccionada).Item("descripcion"),
+                                "Atención!", datosInventario.Rows(FilaSeleccionada).Item("cantidad"))
+        If ibImprevisto = "" Then
+
+        ElseIf Not IsNumeric(ibImprevisto) Then
+            MsgBox("La cantidad tiene que ser numérica")
+        ElseIf ibImprevisto = datosInventario.Rows(FilaSeleccionada).Item("cantidad") Then
+
+        ElseIf IsNumeric(ibImprevisto) Then
+            mysql.InsertarDatos("update inventario set cantidad=" & ibImprevisto & " where ID_INVENTARIO=" &
+                                datosInventario.Rows(FilaSeleccionada).Item("ID_INVENTARIO"))
+            MostrarInventarioEndgv()
+        Else
+            MsgBox("Error al editar el inventario")
+        End If
+
+    End Sub
+
+    Private Sub dgvInventario_SelectionChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dgvInventario.SelectionChanged
+        btnEditarCantidad.Enabled = True
+        btnBorrar.Enabled = True
+    End Sub
+
+    Private Sub btnAgregarDatos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgregarDatos.Click
+        ChequearYAgregarInventario()
+    End Sub
+
+    Private Sub btnBorrar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBorrar.Click
+        borrarInventario()
+    End Sub
+
+    Private Sub btnBorrar_Validating(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles btnBorrar.Validating
+        epError.SetError(dgvInventario, "")
+    End Sub
+
+    Private Sub txtDescripcion_Validating(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles txtDescripcion.Validating
+        epError.SetError(txtDescripcion, "")
+    End Sub
+
+    Private Sub nudCantidad_Validating(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles nudCantidad.Validating
+        epError.SetError(nudCantidad, "")
     End Sub
 End Class
